314dad917509b1b2af68129260760d5f7fc5e0f9
Prathmesh Bahekar <YOUR_GITHUB_EMAIL>
2026-09-14 23:08:47 +0530

    Fix CORS for Vercel frontend

--- BACKEND/main.py
+++ BACKEND/main.py
@@ -11,7 +11,7 @@ load_dotenv()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"],
+    allow_origins=["student-management-iuef-iv89hvezk-dynamo17.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
