9b7f737cae15ba7f6ff8971d2b6d3f7cc898cf6c
Prathmesh Bahekar <YOUR_GITHUB_EMAIL>
2026-09-15 00:01:48 +0530

    Fix Vercel CORS

--- BACKEND/main.py
+++ BACKEND/main.py
@@ -12,7 +12,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://student-management-iuef.vercel.app"
-                   "https://student-management-jpd6.vercel.app"],
+                   "https://student-management-jpd6.vercel.app"
+                   "https://student-management-kefb.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
