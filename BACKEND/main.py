de68a5732d8bf4420bb3aa7719ba25f2cf504128
Prathmesh Bahekar <YOUR_GITHUB_EMAIL>
2026-09-14 23:44:59 +0530

    Fix CORS origin

--- BACKEND/main.py
+++ BACKEND/main.py
@@ -11,7 +11,7 @@ load_dotenv()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["student-management-iuef.vercel.app"],
+    allow_origins=["https://student-management-iuef.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
